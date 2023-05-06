be8df07bd10c96e87a3c037fdfa7a391004a4e14
Neerav Sood <121081642+LoneWalkerNS@users.noreply.github.com>
2023-05-06 05:32:08 -0600

    Update model.py
    Updates to models being used

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.svm import SVR
-import numpy as np
+import numpy as np 
 import joblib 
 
 etf_dir = r"path"
